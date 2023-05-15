37c723d12742f6740a843c2ca036b0c32a9e7c62
Nguyen Thanh Nguyen <ntnguyencse@gmail.com>
2023-05-15 15:09:36 +0000

    Add script delete CAPI Resources
    Signed-off-by: Nguyen Thanh Nguyen <ntnguyencse@gmail.com>

--- hack/delete-stuck-capi-object.sh
+++ hack/delete-stuck-capi-object.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 NAMESPACE = "namespace"
-
-kubectl patch crd/clusters.cluster.x-k8s.io -p  '{"metadata":{"finalizers":[]}}' --type=merge -n $NAMESPACE
-kubectl patch crd/clusterclasses.cluster.x-k8s.io -p  '{"metadata":{"finalizers":[]}}' --type=merge -n $NAMESPACE
+CLUSTER_NAME = "cluster_name"
+kubectl patch crd/clusters.cluster.x-k8s.io -p ${CLUSTER_NAME} '{"metadata":{"finalizers":[]}}' --type=merge -n $NAMESPACE
+kubectl patch crd/clusterclasses.cluster.x-k8s.io -p   '{"metadata":{"finalizers":[]}}' --type=merge -n $NAMESPACE
 kubectl patch crd/machinedeployments.cluster.x-k8s.io -p  '{"metadata":{"finalizers":[]}}' --type=merge -n $NAMESPACE
 kubectl patch crd/openstackmachines.infrastructure.cluster.x-k8s.io  -p  '{"metadata":{"finalizers":[]}}' --type=merge -n $NAMESPACE 
-kubectl patch crd/openstackmachinetemplates.infrastructure.cluster.x-k8s.io -p  '{"metadata":{"finalizers":[]}}' --type=merge -n $NAMESPACE
+kubectl patch crd/openstackmachinetemplates.infrastructure.cluster.x-k8s.io -p ${CLUSTER_NAME}-md-0 '{"metadata":{"finalizers":[]}}' --type=merge -n $NAMESPACE
