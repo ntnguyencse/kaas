15ddc91edc087413103e214c5aa63b7da8c607f6
Nguyen Thanh Nguyen <ntnguyencse@gmail.com>
2023-11-17 01:43:43 +0000

    Add github link

--- hack/patch.sh
+++ hack/patch.sh
@@ -1,6 +1,6 @@
 
 
-kubectl patch machines/edge-2-control-plane-5tlj9 --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
+kubectl patch machines/edge-1-control-plane-vpqsc --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
 kubectl patch openstackcluster/edge-2 --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
 kubectl patch openstackcluster/edge-1 --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
 kubectl patch kubeadmcontrolplanes/mec-1-control-plane --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
@@ -11,8 +11,9 @@ kubectl patch openstackclusters.infrastructure.cluster.x-k8s.io/edge-2 --type js
 
 kubectl patch logicalcluster/intra-edges --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
 
-kubectl patch clusters.cluster.x-k8s.io/mec-1 --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
+kubectl patch clusters.cluster.x-k8s.io/edge-1 --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
 
+kubectl  logs capo-controller-manager-7d454d8df9-2vfb6 -n capo-system
 
 
 
@@ -23,3 +24,6 @@ kubeadmconfigs.bootstrap.cluster.x-k8s.io
 kubeadmcontrolplanes.controlplane.cluster.x-k8s.io
 kubeadmcontrolplanetemplates.controlplane.cluster.x-k8s.io
 openstackclusters.infrastructure.cluster.x-k8s.io
+watch kubectl get pod -A --kubeconfig /tmp/intra-edges/edge-1-kubeconfig.kubeconfig
+watch kubectl get pod -A --kubeconfig /tmp/intra-edges/edge-2-kubeconfig.kubeconfig
+watch kubectl get node --kubeconfig /tmp/intra-edges/edge-2-kubeconfig.kubeconfig
